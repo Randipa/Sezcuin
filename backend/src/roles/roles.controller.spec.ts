b3d28800ee3d96b0cce3506096dd69e77b72bc69
S.P.Randeepa <96324718+Randipa@users.noreply.github.com>
2026-07-09 07:33:54 +0530

    implement CI part and Security check

--- backend/src/roles/roles.controller.spec.ts
+++ backend/src/roles/roles.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
+import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/core/guards/roles.guard';
+import { PermissionsGuard } from 'src/core/guards/permissions.guard';
 
 describe('RolesController', () => {
   let controller: RolesController;
@@ -8,8 +11,15 @@ describe('RolesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RolesController],
-      providers: [RolesService],
-    }).compile();
+      providers: [{ provide: RolesService, useValue: {} }],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: () => true })
+      .overrideGuard(RolesGuard)
+      .useValue({ canActivate: () => true })
+      .overrideGuard(PermissionsGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     controller = module.get<RolesController>(RolesController);
   });
--- backend/src/roles/roles.service.spec.ts
+++ backend/src/roles/roles.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
+import { Role } from './entities/role.entity';
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
+      providers: [
+        RolesService,
+        { provide: getRepositoryToken(Role), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
